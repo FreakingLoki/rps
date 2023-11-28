dd387b5ce74285862ca58c4c349f066c5ba222ae
Jesse <113843920+FreakingLoki@users.noreply.github.com>
2023-11-27 19:46:15 -0500

    update playRound function to use return instead of console.log

--- script.js
+++ script.js
@@ -15,26 +15,27 @@ function getComputerChoice() {
 };
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'cheatcode') {
-        console.log(`the computer chose ${computerSelection} but you annihilated the computer with an orbital laser. You win.`);
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log(`the computer chose ${computerSelection}, but you destroyed them with your big rock. You win.`);
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log(`the computer chose ${computerSelection}, but you encased it in paper. You win.`);
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log(`the computer chose ${computerSelection}, but you shredded it with your scissors. You win.`);
-    } else if (playerSelection === computerSelection) {
-        console.log(`you and the computer assailed each other with your respective choices, but since you both chose ${computerSelection}, nothing came of it. You tied.`);
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log(`the computer wiped your ${playerSelection} off the planet with their roll of paper. You lose.`);
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log(`you attempted to give the computer a mean papercut, but it sliced you instead with its ${computerSelection}. You lose.`);
-    } else if (playerSelection === 'scissors' && computerSelection ==='rock') {
-        console.log(`the computer smashed you to bits with a big ${computerSelection}. How uncivilized. You lose.`)
-    } else {
-        console.log(`something went wrong. You entered ${playerSelection} and the computer chose ${computerSelection}. I dunno, you figure it out.`)
-    }
-}
+    let endText = 'blank';
 
-//code for testing
-console.log(getComputerChoice());
+    if (playerSelection === 'cheat') {
+        endText = `the computer chose ${computerSelection} but you annihilated the computer with an orbital laser. You win.`;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        endText = `the computer chose ${computerSelection}, but you destroyed them with your big rock. You win.`;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        endText = `the computer chose ${computerSelection}, but you encased it in paper. You win.`;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        endText = `the computer chose ${computerSelection}, but you shredded it with your scissors. You win.`;
+    } else if (playerSelection === computerSelection) {
+        endText = `you and the computer assailed each other with your respective choices, but since you both chose ${computerSelection}, nothing came of it. You tied.`;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        endText = `the computer wiped your ${playerSelection} off the planet with their roll of paper. You lose.`;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        endText = `you attempted to give the computer a mean paper cut, but it sliced you instead with its ${computerSelection}. You lose.`;
+    } else if (playerSelection === 'scissors' && computerSelection ==='rock') {
+        endText = `the computer smashed you to bits with a big ${computerSelection}. How uncivilized. You lose.)`
+    } else {
+        endText = `something went wrong. You entered ${playerSelection} and the computer chose ${computerSelection}. I dunno, you figure it out.`
+    }
+
+    return endText;
+}
